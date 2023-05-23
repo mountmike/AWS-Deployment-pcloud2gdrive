5a153c0e16bc51f434c7edd42ddda0c931764f5e
Michael <tharratt.michael@gmail.com>
2023-05-23 16:00:13 +1000

    postgres connection config

--- config/index.js
+++ config/index.js
@@ -1,13 +1,5 @@
 require("dotenv").config()
 
-let missing = ["DATABASE_URL", "GDRIVE_CLIENT_ID", "GDRIVE_CLIENT_SECRETE", "GDRIVE_REDIRECT_URI", "PCLOUD_CLIENT_ID", "PCLOUD_APP_SECRET", "PCLOUD_REDIRECT_URI"].filter(
-  key => process.env[key] === undefined
-)
-
-if (missing.length > 0) {
-  throw Error(`missing environment variables for ${missing.join(", ")}`)
-}
-
 module.exports = {
   port: process.env.PORT,
   db: {
