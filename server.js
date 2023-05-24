63a0e5704d29cb35c6213922d549043975f157d2
Michael <tharratt.michael@gmail.com>
2023-05-24 16:52:13 +1000

    postgres conection variables

--- server.js
+++ server.js
@@ -45,4 +45,8 @@ app.listen(config.port, () => {
   console.log(`listening on port ${config.port}`)
 })
 
-db.query(`select schema_name from information_schema.schemata`).then(response => console.log(response.public + "response"))    
+db.query(`SELECT
+table_name,
+table_schema
+FROM
+information_schema.tables;`).then(response => console.log(response + "response"))    
