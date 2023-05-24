2de3d73eb712f4b4008a3d6d079e3a3ef734a185
Michael <tharratt.michael@gmail.com>
2023-05-24 11:13:48 +1000

    postgres conection variables

--- db/index.js
+++ db/index.js
@@ -1,7 +1,7 @@
-var conString = `postgres://${config.db.user}:${config.db.password}@${config.db.host}:5432/pcloud2gdrive`
-
 const { Pool } = require("pg")
 const config = require("../config")
+
+var conString = `postgres://${config.db.user}:${config.db.password}@${config.db.host}:5432/pcloud2gdrive`
 
 module.exports = new Pool(conString)
 
