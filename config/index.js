fff24d9091463254e21d5306108c0e0438db08a6
Michael <tharratt.michael@gmail.com>
2023-05-24 11:13:15 +1000

    postgres conection variables

--- config/index.js
+++ config/index.js
@@ -3,15 +3,14 @@ require("dotenv").config()
 module.exports = {
   port: process.env.PORT,
   db: {
-    connectionURL: process.env.POSTGRESQL_DB_CONNECTION_STRING,
-    // host: process.env.POSTGRESQL_DB_HOST,
-    // user: process.env.POSTGRESQL_DB_USER,
-    // password: process.env.POSTGRESQL_DB_PASSWORD,
-    // db: process.env.POSTGRESQL_DB,
-    // port: 5432,
-    // dialect: "postgres",
-    // idleTimeoutMillis: 30000,
-    // connectionTimeoutMillis: 3000,
+    host: process.env.POSTGRESQL_DB_HOST,
+    user: process.env.POSTGRESQL_DB_USER,
+    password: process.env.POSTGRESQL_DB_PASSWORD,
+    db: process.env.POSTGRESQL_DB,
+    port: 5432,
+    dialect: "postgres",
+    idleTimeoutMillis: 30000,
+    connectionTimeoutMillis: 3000,
   },
   gDriveAPI: {
     clientId: process.env.GDRIVE_CLIENT_ID,
--- db/index.js
+++ db/index.js
@@ -1,4 +1,8 @@
+var conString = `postgres://${config.db.user}:${config.db.password}@${config.db.host}:5432/pcloud2gdrive`
+
 const { Pool } = require("pg")
 const config = require("../config")
 
-module.exports = new Pool(config.db.connectionURL)
+module.exports = new Pool(conString)
+
+
